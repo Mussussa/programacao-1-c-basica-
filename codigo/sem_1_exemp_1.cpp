1c46aee2bccb85a8485c52818bea53d169e854b6
Mussussa <xismaelchaibo@gmail.com>
2026-05-18 05:00:08 +0200

    first commit - estrutura correta

---
+++ codigo/sem_1_exemp_1.cpp
@@ -0,0 +1,13 @@
+	 // sem_1_exemp_1.cpp
+	// Programa de impressão de texto.
+  	#include <iostream> // permite que o programa gere saída de dados na tela
+  
+ 	// a função main inicia a execução do programa
+  int main()
+ 	{
+ 	std::cout << "Welcome to C++!\n"; // exibe a mensagem
+    
+ 
+  return 0; // indica que o programa terminou com sucesso
+ 
+ 	 } // fim da função main
