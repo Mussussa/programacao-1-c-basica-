9cb200bc49294d93eb81e3c99d041dac06b9106a
Mussussa <xismaelchaibo@gmail.com>
2026-05-19 05:27:33 +0200

    aula 2

--- codigo/sem_1_exemp_1.cpp
+++ codigo/sem_1_exemp_1.cpp
@@ -1,13 +1,21 @@
 	 // sem_1_exemp_1.cpp
 	// Programa de impressão de texto.
   	#include <iostream> // permite que o programa gere saída de dados na tela
+  	int suma(int a, int b);
   
- 	// a função main inicia a execução do programa
+ 	// a funcao main inicia a execução do programa
   int main()
  	{
+ 		
  	std::cout << "Welcome to C++!\n"; // exibe a mensagem
+ 	int suma_result = suma(1 , 7);
+ 	std::cout << suma_result; // exibe a mensagem
     
  
   return 0; // indica que o programa terminou com sucesso
  
- 	 } // fim da função main
+ 	 } // fim da funcao main
+ 	 
+	 int suma(int a , int b){
+	 	return a + b ;
+	 }
